0063d0f76b3d3430f0df2cdc456e1f68363b77ee
Christopher <christopher.diercksen@gmail.com>
2022-01-25 17:38:22 -0500

    destroyed incomplete Treasure resource

--- app/controllers/treasures_controller.rb
+++
@@ -1,2 +0,0 @@
-class TreasuresController < ApplicationController
-end
--- db/migrate/20220125223543_create_treasures.rb
+++
@@ -1,8 +0,0 @@
-class CreateTreasures < ActiveRecord::Migration[7.0]
-  def change
-    create_table :treasures do |t|
-
-      t.timestamps
-    end
-  end
-end
