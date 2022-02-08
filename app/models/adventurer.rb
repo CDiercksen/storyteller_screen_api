4b73dca7d2bb96236f6bbd1aaa68165c2a002251
Christopher <christopher.diercksen@gmail.com>
2022-02-08 14:58:36 -0500

    customized Adventurer validation- requires integer attributes to be greater than or equal to 1

--- app/models/adventurer.rb
+++ app/models/adventurer.rb
@@ -1,3 +1,4 @@
 class Adventurer < ApplicationRecord
-    validates :name, :armor_class, :hit_point_maximum, :spell_dc, presence: true
+    validates :name, presence: true
+    validates :armor_class, :hit_point_maximum, :spell_dc, numericality: {greater_than_or_equal_to: 1}
 end
